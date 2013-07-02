029e272bf2e5ba7cf3a8baf0045d31e7369448c8
Han Chang <szu.han.chang@gmail.com>
2013-07-02 07:12:22 -0700

    Virgin Rails 3.2.12 installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReversePolishNotation::Application.config.secret_token = '09fbd143dcb37bd55084883a8c1590b562e9dbf26927304b9a14f6c6836b139fdd44140bb88954a70be762798963dce56b6b68bd6bade6435172e120c0e6432b'
